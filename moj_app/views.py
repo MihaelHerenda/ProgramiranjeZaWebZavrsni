b453315f5bc18a126772fde7d2dc20d9fee911c7
secretcherry <juliahozjan1@gmail.com>
2026-01-31 22:37:38 +0100

    Kreirani funkcionalni Comment i Like

--- moj_app/views.py
+++ moj_app/views.py
@@ -1,40 +1,74 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from .models import Post, Like, Comment
+from .forms import PostForm, CommentForm 
 
-# 1. REGISTRACIJA
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login') # Nakon reg. šalji na login
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# 2. LISTA POSTOVA (Naslovnica)
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at') # Najnoviji prvi
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/post_list.html', {'posts': posts})
 
-# 3. DETALJI POSTA
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'posts/post_detail.html', {'post': post})
-
-# 4. KREIRANJE POSTA (Samo za logirane)
 @login_required
 def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user # Automatski dodaj autora
+            post.author = request.user
             post.save()
             return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'posts/post_form.html', {'form': form})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all() 
+    
+    is_liked = False
+    if request.user.is_authenticated:
+        if post.likes.filter(user=request.user).exists():
+            is_liked = True
+
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('login') 
+            
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'posts/post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+        'is_liked': is_liked 
+    })
+
+@login_required
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
+    
+    if not created:
+        like.delete()
+        
+    return redirect('post_detail', pk=pk)
