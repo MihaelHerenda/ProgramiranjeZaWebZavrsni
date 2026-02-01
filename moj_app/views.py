67164acc35466ff36be23791a41538f40c9e14a4
secretcherry <juliahozjan1@gmail.com>
2026-02-01 17:36:15 +0100

    Prebacivanje na Generic views

--- moj_app/views.py
+++ moj_app/views.py
@@ -1,112 +1,128 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from .models import Post, Like, Comment
-from .forms import PostForm, CommentForm 
+from django.contrib.auth import login
 from django.db.models import Q
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Like, Comment
+from .forms import PostForm, CommentForm
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            return redirect('post_list')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-def post_list(request):
-    query = request.GET.get('q') # Dohvati ono što je upisano u tražilicu
-    
-    if query:
-        # Filtriraj po naslovu ILI sadržaju
-        posts = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        ).order_by('-created_at')
-    else:
-        posts = Post.objects.all().order_by('-created_at')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['form'] = CommentForm()
         
-    return render(request, 'posts/post_list.html', {'posts': posts, 'query': query})
+        is_liked = False
+        if self.request.user.is_authenticated:
+            if self.object.likes.filter(user=self.request.user).exists():
+                is_liked = True
+        context['is_liked'] = is_liked
+        return context
 
-@login_required
-def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('post_list')
-    else:
-        form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': form})
-
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    comments = post.comments.all() 
     
-    is_liked = False
-    if request.user.is_authenticated:
-        if post.likes.filter(user=request.user).exists():
-            is_liked = True
-
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect('login') 
+            return redirect('login')
             
+        self.object = self.get_object()
         form = CommentForm(request.POST)
+        
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            comment.post = self.object
             comment.author = request.user
             comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
+            return redirect('post_detail', pk=self.object.pk)
+        
+        context = self.get_context_data(object=self.object)
+        context['form'] = form
+        return self.render_to_response(context)
 
-    return render(request, 'posts/post_detail.html', {
-        'post': post,
-        'comments': comments,
-        'form': form,
-        'is_liked': is_liked 
-    })
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    success_url = reverse_lazy('post_list')
 
-@login_required
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_edit'] = True 
+        return context
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+    template_name = 'posts/post_confirm_delete.html' 
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
 def like_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    like, created = Like.objects.get_or_create(user=request.user, post=post)
-    
-    if not created:
-        like.delete()
-        
+    if request.user.is_authenticated:
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if not created:
+            like.delete()
     return redirect('post_detail', pk=pk)
-
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    
-   
-    if request.user != post.author:
-        return redirect('post_detail', pk=pk)
-
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post) 
-        if form.is_valid():
-            form.save()
-            return redirect('post_detail', pk=pk)
-    else:
-        form = PostForm(instance=post)
-    
-    
-    return render(request, 'posts/post_form.html', {'form': form, 'is_edit': True})
-
-@login_required
-def post_delete(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    
-    if request.user == post.author:
-        post.delete()
-        
-    return redirect('post_list')
--- moj_projekt/urls.py
+++ moj_projekt/urls.py
@@ -2,22 +2,26 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views 
 from moj_app import views 
+from moj_app.views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView, 
+    PostDeleteView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # AUTH 
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # POST 
-    path('', views.post_list, name='post_list'), 
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_create, name='post_create'),
-
+    path('', PostListView.as_view(), name='post_list'), 
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    
     path('post/<int:pk>/like/', views.like_post, name='like_post'),
-
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
